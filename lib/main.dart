66723136602b270225ea8641be6a27be2e86111d
Melmohands22 <melmohands591@gmail.com>
2025-04-28 16:15:38 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const DayTracker());
+}
+
+class DayTracker extends StatelessWidget {
+  const DayTracker({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
